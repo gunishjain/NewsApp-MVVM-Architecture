308aac0b759c77674c46f2e83e00b44951feee54
gunishjain <gunish.jain@gmail.com>
2023-11-27 16:01:01 +0530

    Add Readme

--- app/src/main/java/com/gunishjain/newsapp/utils/AppConstant.kt
+++ app/src/main/java/com/gunishjain/newsapp/utils/AppConstant.kt
@@ -2,7 +2,7 @@ package com.gunishjain.newsapp.utils
 
 object AppConstant {
     const val APP_NAME = "NewsApp"
-    const val API_KEY = "2f8b8962d0384575af31c5ffe7f2c903"
+    const val API_KEY = "YOUR_API_KEY"
     const val COUNTRY = "in"
     const val LANGUAGE = "en"
     const val BASE_URL = "https://newsapi.org/v2/"
